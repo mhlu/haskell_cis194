ffd4196ce461697bb29b95d8a529188883dbdea8
mhlu <mhlu@uwaterloo.ca>
2018-09-24 23:06:30 -0400

    Reworked w3

--- w3/Golf.hs
+++ w3/Golf.hs
@@ -1,23 +1,37 @@
 module Golf where
+
 import Data.List
+
+
+skips :: [a] -> [[a]]
+skips xs = map (flip skip xs) $ [1..length xs]
+    where skip n = map snd . filter ((==n) . fst ) . zip (cycle [1..n])
+
+{- this version is shorter
 
 skips :: [a] -> [[a]]
 skips xs = map (skip xs) [1..length xs]
     where skip xs n = case drop (n-1) xs of
                 (y:ys) -> y : skip ys n
-                [] -> []
-
+                [] -> [] -}
 
 localMaxima :: [Integer] -> [Integer]
-localMaxima = reverse . sort . getMaxs
-    where getMaxs (a:b:c:xs) 
-            | b > a && b > c = b : getMaxs (c:xs)
-            | otherwise = getMaxs (b:c:xs)            
-          getMaxs _ = []
+localMaxima (a:b:c:xs)
+    | b > a && b > c = b : localMaxima (c:xs)
+    | otherwise = localMaxima (b:c:xs)
+localMaxima _ = []
+
+{-
+localMaxima :: [Integer] -> [Integer]
+localMaxima xs@(x:_) = map fst . filter (snd) . zip xs . zipWith (&&) left $ right
+    where left = zipWith (>) xs (x:init xs)
+          right = zipWith (>) xs (tail xs) ++ [False] -}
+
 
 histogram :: [Integer] -> String
-histogram xs = (unlines . transpose $ hori) ++ "==========\n0123456789\n"
+histogram xs = (unlines . transpose $ cols ) ++ "==========\n0123456789\n"
     where count xs n = length . filter (==n) $ xs
           counts = map (count xs) [0..9]
-          maxCount = maximum counts
-          hori = map (\n -> reverse $ replicate n '*' ++ replicate (maxCount - n) ' ') counts
+          maxCnt = maximum counts
+          col n = replicate (maxCnt - n) ' ' ++ replicate n '*'
+          cols = map col counts
